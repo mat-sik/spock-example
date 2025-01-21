fd4ce121430e79d53b76d6fba62cb1ba964668f6
mat-sik <siiikor.sikorski@gmail.com>
2025-01-21 12:40:27 +0100

    Add parametrized test

--- src/test/groovy/com/github/matsik/CalculatorSpec.groovy
+++ src/test/groovy/com/github/matsik/CalculatorSpec.groovy
@@ -1,16 +1,20 @@
 package com.github.matsik
 
-
 import spock.lang.Specification
 
 class CalculatorSpec extends Specification {
 
-    def "add should correctly sum two numbers"() {
-        given: "a calculator instance"
-        def calculator = new Calculator()
+    def calculator = new Calculator()
 
+    def "add should correctly sum two numbers"() {
         expect: "the add result should be their sum"
-        calculator.add(2, 3) == 5
+        calculator.add(a, b) == c
+
+        where:
+        a | b || c
+        1 | 2 || 3
+        2 | 2 || 4
+        3 | 2 || 5
     }
 
 }
