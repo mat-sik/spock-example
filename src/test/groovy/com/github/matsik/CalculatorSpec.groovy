fae4e9afaf1a8216380677d4833ce2f0420b7151
mat-sik <siiikor.sikorski@gmail.com>
2025-01-21 14:34:17 +0100

    Add simple mock to study the api

--- src/test/groovy/com/github/matsik/CalculatorSpec.groovy
+++ src/test/groovy/com/github/matsik/CalculatorSpec.groovy
@@ -4,9 +4,13 @@ import spock.lang.Specification
 
 class CalculatorSpec extends Specification {
 
-    def calculator = new Calculator()
-
     def "add should correctly sum two numbers"() {
+        given:
+        Calculator calculator = Mock {
+            1 * add(_, _) >> {first, second -> first + second}
+            0 * _
+        }
+
         expect: "the add result should be their sum"
         calculator.add(a, b) == c
 
@@ -15,6 +19,7 @@ class CalculatorSpec extends Specification {
         1 | 2 || 3
         2 | 2 || 4
         3 | 2 || 5
+        2 | 3 || 5
     }
 
 }
